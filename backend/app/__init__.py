e505527961647a753b9b26989b987c19fe4953a4
SoonAnthony <anthonymedia062@gmail.com>
2026-02-24 09:02:05 +0300

    We have just created a lifespan event that when the app runs, it connects to the database and disconnects with the db just right before shutdown

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,21 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from sqlmodel import SQLModel
+from .db.engine import engine
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup logic
+    print("Connecting to the database...")
+    async with engine.begin() as conn:
+        await conn.run_sync(lambda _: None)
+    print("Connecting to the database.")
+    yield
+    # shutdown logic
+    print("Disconnecting from the database...")
+    await engine.dispose()
+    print("Disconnected from the database.")
+
+app = FastAPI(lifespan=lifespan)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
