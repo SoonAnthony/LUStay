4f49328f8dcb2301b751a593555d742dbb002e09
SoonAnthony <anthonymedia062@gmail.com>
2026-02-24 09:18:10 +0300

    Instead of a dummy lambda we ran a lightweight SQl which is more explicit and easier to debug

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,14 +2,15 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from sqlmodel import SQLModel
 from .db.engine import engine
+from sqlalchemy import text
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # startup logic
     print("Connecting to the database...")
     async with engine.begin() as conn:
-        await conn.run_sync(lambda _: None)
-    print("Connecting to the database.")
+         await conn.execute(text("SELECT 1"))
+    print("Connected to the database.")
     yield
     # shutdown logic
     print("Disconnecting from the database...")
