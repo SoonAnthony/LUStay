bc30f5f31d70a719e3c61f3496eba72643dad7fb
SoonAnthony <anthonymedia062@gmail.com>
2026-02-24 15:50:37 +0300

    updated my engine to industry level and to work well with the config.py file

--- backend/app/db/engine.py
+++ backend/app/db/engine.py
@@ -1,10 +1,15 @@
 from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
-from sqlalchemy.ext.asyncio import create_async_engine
-from db.engine import settings
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+from app.core.config import settings
 
-engine = create_async_engine(
+engine: AsyncEngine = create_async_engine(
     settings.DATABASE_URL,
     echo=True,
     future = True
 )
+
+async def get_session() -> AsyncSession:
+    async with AsyncSession(engine) as session:
+        yield session
+
