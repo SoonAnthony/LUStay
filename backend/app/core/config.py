f3aebe37a2ee6ef92f592fb06ae23196bf02700c
SoonAnthony <anthonymedia062@gmail.com>
2026-02-24 15:32:35 +0300

    Just updated my config.py file to production level to work with my neon db

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,8 +1,22 @@
 from pydantic_settings import BaseSettings
-
+from pathlib import Path
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int = 5432
+    DB_NAME: str
+    DB_SSL_MODE: str = "require"
+
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}?sslmode={self.DB_SSL_MODE}"
+    
+
+    class Config:
+        env_file = Path(__file__).parent.parent / ".env"
 
 
 settings = Settings()
