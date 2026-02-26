ddce405c9989e307dc57acd4ff35bba06cd25d34
SoonAnthony <anthonymedia062@gmail.com>
2026-02-26 23:53:49 +0300

    Updated to the userschema so that I can use inheritance

--- backend/app/user/schema.py
+++ backend/app/user/schema.py
@@ -8,14 +8,8 @@ class UserSchema(BaseModel):
     id: uuid.UUID
     first_name: str
     last_name: str
-    email: EmailStr
-    phone_number: str
-    profile_image: Optional[str] | None
-    role: UserRole
-    is_verified: bool
-    is_suspended: bool
-    created_at: datetime
-    updated_at: datetime
-    last_login: Optional[datetime] | None
+    profile_image: Optional[str] = None
 
     model_config = ConfigDict(from_attributes=True)
+
+
