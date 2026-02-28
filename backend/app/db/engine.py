88ba93724c81f75a17bebece8a7fb16fd083dbed
SoonAnthony <anthonymedia062@gmail.com>
2026-02-28 15:52:15 +0300

    Changed the funcion to return AsyncGenerator[AsyncSession, None] for production clarity, however the previous one would still work well

--- backend/app/db/engine.py
+++ backend/app/db/engine.py
@@ -2,6 +2,7 @@ from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 from app.core.config import settings
+from typing import AsyncGenerator
 
 engine: AsyncEngine = create_async_engine(
     settings.DATABASE_URL,
@@ -9,7 +10,7 @@ engine: AsyncEngine = create_async_engine(
     future = True
 )
 
-async def get_session() -> AsyncSession:
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSession(engine) as session:
         yield session
 
