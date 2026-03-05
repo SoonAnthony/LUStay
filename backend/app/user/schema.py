b25be3522d4bf415a09b5feb2ee0e2cead7b4da9
SoonAnthony <anthonymedia062@gmail.com>
2026-03-05 23:38:20 +0300

    Added the login Schema

--- backend/app/user/schema.py
+++ backend/app/user/schema.py
@@ -130,3 +130,7 @@ class TokenResponse(BaseModel):
     access_token: str
     refresh_token: str
     token_type: str = "bearer"
+
+class LoginSchema(BaseModel):
+    email: EmailStr
+    password: str = Field(..., min_length=8, max_length=128)
