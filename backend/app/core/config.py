eafe871f6565e13d41de75a9b9db956c43c787e2
SoonAnthony <anthonymedia062@gmail.com>
2026-03-09 15:51:54 +0300

    Added the cloudinary configs

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,17 +2,22 @@ from pydantic_settings import BaseSettings
 from pathlib import Path
 
 class Settings(BaseSettings):
+    #Database configuration
     DB_USER: str
     DB_PASSWORD: str
     DB_HOST: str
     DB_PORT: int = 5432
     DB_NAME: str
     DB_SSL_MODE: str = "require"
+    #Security configuration
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     REFRESH_TOKEN_EXPIRE_DAYS: int
-
+    #Cloudinary configuration
+    CLOUDINARY_CLOUD_NAME: str
+    CLOUDINARY_API_KEY: str
+    CLOUDINARY_API_SECRET: str
 
     @property
     def DATABASE_URL(self) -> str:
