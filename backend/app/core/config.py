abfff294a891afaeb5230bf5000c41642196dddb
SoonAnthony <anthonymedia062@gmail.com>
2026-03-09 15:54:32 +0300

    Removed the duplication on DB_PORT

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASSWORD: str
     DB_HOST: str
-    DB_PORT: int = 5432
+    DB_PORT: int 
     DB_NAME: str
     DB_SSL_MODE: str = "require"
     #Security configuration
