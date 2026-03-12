1a007d5e303bf085f37175658dbb5ae2e1954d4d
SoonAnthony <anthonymedia062@gmail.com>
2026-03-13 00:40:45 +0300

    Removed onupdate=func.now(), so that last login doesn't trigger the last update

--- backend/app/user/models.py
+++ backend/app/user/models.py
@@ -71,7 +71,6 @@ class User(SQLModel, table=True):
         sa_column=Column(
             DateTime(timezone=True),
             server_default=func.now(),
-            onupdate=func.now(),
             nullable=False
         )
     )
