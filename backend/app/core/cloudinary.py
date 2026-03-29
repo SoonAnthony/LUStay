31ac63e1381ef802ce29d20abaafa1f1d4eec9e6
SoonAnthony <anthonymedia062@gmail.com>
2026-03-30 00:47:36 +0300

    Cleaned this file after it worked

--- backend/app/core/cloudinary.py
+++ backend/app/core/cloudinary.py
@@ -2,10 +2,7 @@ from app.core.config import settings
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-import os
-
-print("CLOUDINARY_URL from env:", os.getenv("CLOUDINARY_URL"))
 
 cloudinary.config(secure=True)
 
-print("Cloudinary config check:", cloudinary.config())
+
