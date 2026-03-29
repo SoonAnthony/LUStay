9c54958fa9e6b252b1165e26b96ad6f3586ca9ba
SoonAnthony <anthonymedia062@gmail.com>
2026-03-30 00:45:49 +0300

    Had to test if my env were being loaded

--- backend/app/core/cloudinary.py
+++ backend/app/core/cloudinary.py
@@ -1,5 +1,11 @@
 from app.core.config import settings
 import cloudinary
 import cloudinary.uploader
+import cloudinary.api
+import os
+
+print("CLOUDINARY_URL from env:", os.getenv("CLOUDINARY_URL"))
 
 cloudinary.config(secure=True)
+
+print("Cloudinary config check:", cloudinary.config())
