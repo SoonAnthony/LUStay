7358172f06dda6c48e9f83063d6f7814aa4e30f3
SoonAnthony <anthonymedia062@gmail.com>
2026-03-31 07:35:35 +0300

    Added the schema for the bookings domain

---
+++ backend/app/bookings/schema.py
@@ -0,0 +1,29 @@
+import uuid
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict
+
+class BookingCreate(BaseModel):
+    room_id: uuid.UUID
+    semester: str
+    is_shared: bool
+
+class BookingUpdate(BaseModel):
+    room_id: uuid.UUID | None = None
+    semester: str | None = None
+    status: str | None = None
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class BookingRead(BaseModel):
+    id: uuid.UUID
+    user_id: uuid.UUID
+    room_id: uuid.UUID
+    semester: str
+    is_shared: bool
+    total_price: int
+    deposit_amount: int
+    amount_paid: int
+    status: str
+    created_at: datetime
+    model_config = ConfigDict(from_attributes=True)
