9d3e40b0c536f2b554ecf378b6c26b5004ba7d99
SoonAnthony <anthonymedia062@gmail.com>
2026-04-10 22:50:02 +0300

    Added the home page content

---
+++ frontend/client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-3xl font-bold">Welcome to LUStay</h1>
+    </div>
+  );
+};
+
+export default Home;
