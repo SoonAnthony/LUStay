bf11616dd362130f9ef9c04db312ba4a60d7c0bb
SoonAnthony <anthonymedia062@gmail.com>
2026-04-10 22:54:44 +0300

    Added the hostels page

---
+++ frontend/client/src/pages/Hostels.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hostels = () => {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-2xl font-bold">Hostels Page</h1>
+    </div>
+  );
+};
+
+export default Hostels;
