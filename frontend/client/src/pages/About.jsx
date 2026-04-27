a725394d9d78ff5f608f72bf8fcb77412ebdb530
SoonAnthony <anthonymedia062@gmail.com>
2026-04-27 07:13:05 +0300

    Search bar can also search by location

--- frontend/client/src/pages/About.jsx
+++ frontend/client/src/pages/About.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import AboutUs from "../components/AboutUs";
+import Footer from "../components/Footer";
 
 const About = () => {
   return (
-    <div className="text-center mt-10">
-      <h1 className="text-3xl font-bold">About LUStay</h1>
-    </div>
+    <>
+      <AboutUs />
+      <Footer />
+    </>
   );
 };
 
