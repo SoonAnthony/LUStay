b7e612c6be9e8aa3947381295eff6cf87071bcc3
SoonAnthony <anthonymedia062@gmail.com>
2026-05-02 16:22:31 +0300

    Now suspended admin cannot have admin priviledges

--- backend/app/user/dependencies.py
+++ backend/app/user/dependencies.py
@@ -65,7 +65,7 @@ async def get_current_active_user(
 
 
 async def get_current_admin(
-    current_user: User = Depends(get_current_user)
+    current_user: User = Depends(get_current_active_user) 
 ) -> User:
     if current_user.role != UserRole.ADMIN:
         raise HTTPException(
